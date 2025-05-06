2c25e496b4f3c1df78fae6f9796621ac6b660b01
Otavio <estudosprogotavio@gmail.com>
2025-05-06 10:23:30 -0300

    feat:entidade paciente

---
+++ models/paciente.py
@@ -0,0 +1,6 @@
+class Paciente:
+    def __init__(self, nome, cpf, data_nascimento, telefone):
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+        self.telefone = telefone
